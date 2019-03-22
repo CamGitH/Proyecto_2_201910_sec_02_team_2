5f801a2a63c0c7957f249e53ce63913a3b8f2ce8
Silviaserrano <46981148+Silviaserrano@users.noreply.github.com>
2019-03-22 13:17:20 -0500

    tests

---
+++ testSource/test/TestColaPrioridad.java
@@ -0,0 +1,71 @@
+/**
+ * 
+ */
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author Tomás Langebaek
+ */
+public class TestColaPrioridad {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link model.data_structures.ColaPrioridadHeap#darNumElementos()}.
+	 */
+	@Test
+	public void testDarNumElementos() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link model.data_structures.ColaPrioridadHeap#tomarElemento()}.
+	 */
+	@Test
+	public void testTomarElemento() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link model.data_structures.ColaPrioridadHeap#agregar(java.lang.Comparable)}.
+	 */
+	@Test
+	public void testAgregar() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link model.data_structures.ColaPrioridadHeap#estaVacia()}.
+	 */
+	@Test
+	public void testEstaVacia() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link model.data_structures.ColaPrioridadHeap#delMax()}.
+	 */
+	@Test
+	public void testDelMax() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link model.data_structures.ColaPrioridadHeap#max()}.
+	 */
+	@Test
+	public void testMax() {
+		fail("Not yet implemented");
+	}
+
+}
